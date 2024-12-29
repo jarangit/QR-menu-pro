41b83e46ec103e2f573ef09bd8971f84d9b2b94e
Jaran Donchaaim <>
2024-12-30 02:46:03 +0700

    feat: add image domain for external images and enhance food card styling

--- src/app/ui-system/molecules/food-card.tsx
+++ src/app/ui-system/molecules/food-card.tsx
@@ -5,6 +5,7 @@ import { IoArrowRedo } from "react-icons/io5";
 import { MdFastfood, MdLocationOn } from "react-icons/md";
 
 const FoodCard = () => {
+  // const image = "https://img.freepik.com/free-photo/chocolate-smoothie_1339-2860.jpg?t=st=1735501019~exp=1735504619~hmac=565a9021f6e47f20e59dc251b5019b8d5b56edf46a971443d05bc3539a3ed084&w=1060";
   const image = "/assets/images/food1.jpg";
   const dot = ["", "", "", "", ""];
   return (
@@ -18,9 +19,11 @@ const FoodCard = () => {
 
           {/* km */}
           <div className="absolute left-4 top-4 ">
-            <div className="bg-white/20 text-white/70 text-sm font-bold h-8 flex justify-center items-center px-2 rounded-full gap-1 ">
+            <div className="bg-white/20 backdrop-blur-sm shadow-sm text-black/50 text-sm font-bold h-8 flex justify-center items-center px-2 rounded-full gap-1 ">
               <MdLocationOn size={15} />
-              <div>1 km</div>
+              <div className="" >
+                1 km
+              </div>
             </div>
           </div>
 
